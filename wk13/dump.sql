10a950be8534afb55789efb23bd74b843b7d119b
saharapanna <43882742+saharapanna@users.noreply.github.com>
2020-04-12 01:16:29 -0400

    Week 13 completed

---
+++ wk13/dump.sql
@@ -0,0 +1,59 @@
+mysqldump -u root -p Lab9_Schema > dump.sql
+
+ mysqldump -u root -p Lab9_Schema users
+Enter password:
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: Lab9_Schema
+-- ------------------------------------------------------
+-- Server version       5.7.29-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `active` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Tigar','tiger@mail.com','Tiger','Shroff',1),(2,'John','john@gmail.com','John','Abraham',0),(3,'Kevin','Kev@gmail.com','Kevin','Samule',0),(4,'Sara','sara@gmail.com','Sara','Martin',1),(5,'Mike','mike@gmail.com','Mike','Brian',1),(6,'benblanc','benblanc@gbc.ca','Ben','Blanc',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-12  5:00:42
---
+++ wk13/getusers_1.php
@@ -0,0 +1,32 @@
+<form method="GET">
+    <input type=text name=input>
+    <input type=submit name=submit value="Submit">
+</form>
+
+<?php
+$link=mysqli_connect("localhost","my_user","my_passw","my_db");
+
+if (mysqli_connect_errno())
+    echo "Failed to connect to the database: " . mysqli_connect_error();
+
+if (isset($_GET["submit"])){
+   $result=mysqli_query($link, "SELECT * FROM users WHERE firstname LIKE '" . $_GET['input'] . "' AND active LIKE 1");
+    echo "<table border='2' cellspacing='10'>
+    <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    </tr>";
+
+    while($row = mysqli_fetch_array($result))
+    {
+    echo "<tr>";
+    echo "<td>" . $row['firstname'] . "</td>";
+    echo "<td>" . $row['lastname'] . "</td>";
+    echo "</tr>";
+    }
+    echo "</table>";
+}
+mysqli_close($link);
+
+?>
+
